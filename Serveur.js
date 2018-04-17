6ffe63eae131c2f80ccd0d076350b647436f52d1
Unknown <flolevy33@gmail.com>
2018-04-17 01:28:58 -0400

    creation socket envoie et reception variables

--- Serveur.js
+++ Serveur.js
@@ -9,3 +9,18 @@ app.get('/', function(req,res)
 app.use('/client', express.static(__dirname + '/client'));
 
 http.listen(2000);
+console.log("Server started");
+
+var io = require('socket.io')(http,{});
+io.sockets.on('connection', function(socket)
+{
+    console.log('socket connection');
+
+    socket.on('happy', function(data){
+        console.log('happy because ' + data.reason);
+    });
+
+    socket.emit('serveurMsg',{
+        msg:'hello',
+    });
+});
